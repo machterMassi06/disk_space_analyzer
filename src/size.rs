3948d47c0b032671a08b98df917c3baa0cb1a302
Massinissa MACHTER <massinissa.machter@gmail.com>
2024-11-03 14:07:11 +0100

    Implementation du trait fmt::Display pour Size et tests unitaires

--- src/size.rs
+++ src/size.rs
@@ -11,7 +11,15 @@ impl Size {
 
 impl fmt::Display for Size {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
+        let value = self.0 as f64;
+        // formattons la valeur a une unite adaptée
+        let (formatted_value,suffix)= match value {
+            1_048_576.0 .. => (value /1_048_576.0,"MiB"),
+            1_024.0 .. => (value/1_024.0,"KiB"),
+            _ => (value,"bytes"), 
+        };
+        // Affiche le résultat avec une précision de 1 décimale
+        write!(f, "{:.1} {}", formatted_value, suffix)
     }
 }
 
@@ -32,5 +40,20 @@ pub mod tests{
         let s2=Size::new(64);
         let res = s1+s2;
         assert_eq!(res.0,80);
+    }
+    #[test]
+    fn test_display_bytes(){
+        let size=Size::new(500);
+        assert_eq!(format!("{}",size),"500.0 bytes");
+    }
+    #[test]
+    fn test_display_kib(){
+        let size=Size::new(1024);
+        assert_eq!(format!("{}",size),"1.0 KiB");
+    }
+    #[test]
+    fn test_display_mib(){
+        let size=Size::new(2411724);
+        assert_eq!(format!("{}",size),"2.3 MiB");
     }
 } 
