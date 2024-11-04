469c19d58c51958743e3aabd0d53a71147c9cfaf
Massinissa MACHTER <massinissa.machter@gmail.com>
2024-11-04 20:45:00 +0100

    implementation de la methode show de FileTree

--- src/print_tree.rs
+++ src/print_tree.rs
@@ -1,7 +1,28 @@
+use std::path::PathBuf;
+
 use crate::file_tree::FileTree;
 
 impl FileTree {
     pub fn show(&self) {
-        unimplemented!()
+        let path = PathBuf::from(self.get_root());
+        self.show_node(&path,0);
+    }
+
+    pub fn show_node(&self,path:&PathBuf,level:usize){
+        if let Some(_node)=self.get_node(path){
+            let indent="    ".repeat(level);
+            if let Some(size)=self.get_size(path){
+                println!("{}{} {}{}",indent, size, indent, path.display());
+            }
+            match self.get_children(path){
+                Some(children)=> {
+                    for child in children{
+                        self.show_node(child, level+1);
+                    }
+                },
+                None => {},
+            }
+
+        }
     }
 }
